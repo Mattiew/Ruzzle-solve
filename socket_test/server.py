f8fcffc69f2fe6ef2686fedeb8059d8a416c625e
Mattiew <50993030+Mattiew@users.noreply.github.com>
2019-06-11 16:24:25 +0200

    Update server.py

--- socket_test/server.py
+++ socket_test/server.py
@@ -18,17 +18,20 @@ print("Le serveur écoute à présent sur le port {}".format(port))
 connexion_avec_client, infos_connexion = connexion_principale.accept()
 
 msg_recu = b""
-while msg_recu != b"fin":
+while msg_recu != b"fin" or msg_a_envoyer != b"fin":
     msg_recu = connexion_avec_client.recv(1024)
     # L'instruction ci-dessous peut lever une exception si le message
     # Réceptionné comporte des accents
-    print(msg_recu.decode())
+    print('client : {}'.format(msg_recu.decode()))
     connexion_avec_client.send(b"----------------------recu----------------------")
-    msg_a_envoyer = input("> ")
-    msg_a_envoyer = msg_a_envoyer.encode()
-    connexion_avec_client.send(msg_a_envoyer)
-    msg_recu = connexion_avec_client.recv(1024)
-    print(msg_recu.decode())
+    
+    if msg_recu != b"fin":
+        msg_a_envoyer = input("moi : ")
+        msg_a_envoyer = msg_a_envoyer.encode()
+        connexion_avec_client.send(msg_a_envoyer)
+        msg_recu = connexion_avec_client.recv(1024)
+        print(msg_recu.decode())
+        
 print("Fermeture de la connexion")
 connexion_avec_client.close()
 connexion_principale.close()
